68cdaf8099084cf01d43266ef5a702191381c39b
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-08-28 11:23:29 +0200

    add first fobos

---
+++ src/app/off.f90
@@ -0,0 +1,12 @@
+!< OFF, Open source Finite volumes Fluid dynamics code.
+
+program off
+!< OFF, Open source Finite volumes Fluid dynamics code.
+! use off_objects, only : simulation_object
+
+implicit none
+! type(simulation_object) :: simulations !< Simulation data.
+
+! call simulations%initialize
+
+endprogram off
